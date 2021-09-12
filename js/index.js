1748edb0e069abd1aa071ca6d53b0799d1b030e6
me-mulet <mercedes.mulet@gmail.com>
2021-09-12 14:50:45 -0500

    implementando ajax y apis

--- js/index.js
+++ js/index.js
@@ -8,9 +8,9 @@ $(function () {
 
     $('.card-home').hide().delay(1500).fadeIn(2000);
 
-    $(".top-btn").click( () => {
+    $(".top-btn").click(() => {
         $('html, body').animate({
-            scrollTop: $("body").offset().top  
+            scrollTop: $("body").offset().top
         }, 1000);
     });
 
@@ -19,28 +19,29 @@ $(function () {
         left: '0px',
     }, 1500)
 
+    //Ajax y apis
     //Declaramos la url que vamos a usar para el GET
     const URLGET = "https://jsonplaceholder.typicode.com/posts"
     //Escuchamos el evento click del botón agregado
-    $("#testimonialBtn").click(() => { 
+    $("#testimonialBtn").click(() => {
         console.log("entro al evento click");
         $.get(URLGET, function (respuesta, estado) {
             console.log(estado);
             console.log(respuesta);
-             if(estado === "success"){
+            if (estado === "success") {
                 let misDatos = respuesta;
-                for (let i=0; i < 3; i++) {
-                    $("main").append(`
-                                <div class="card">
-                                    <h5 class="card-header"> ${misDatos[i].id}</h5>
-                                    <div class="card-body">
-                                        <h5 class="card-title">${misDatos[i].title}</h5>
-                                        <p class="card-text">${misDatos[i].body}</p>
-                                    </div>
-                                    <figure><img src='./img/${i}.jpg'></figure>
-                                </div>`);
-                }  
-            } 
+                for (let i = 0; i < 3; i++) {
+                    $("#testimonials").append(`
+                        <div class="container card">
+                            <figure><img src='./img/${i}.jpg'></figure>
+                            <div class="card-body">
+                                <h6 class="card-id"> ${misDatos[i].id}</h6>
+                                <h5 class="card-title">${misDatos[i].title}</h5>
+                                <p class="card-text">"${misDatos[i].body}"</p>
+                            </div>
+                        </div>`);
+                }
+            }
         });
         $("#testimonialBtn").remove();
     });
